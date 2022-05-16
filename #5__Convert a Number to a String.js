1fc9ce788a03d2842aa61f2d6e0016505d808d26
Dmitriy <groont@list.ru>
2022-05-16 14:54:07 +0300

    add #5__Convert a Number to a String!

---
+++ #5__Convert a Number to a String.js
@@ -0,0 +1,5 @@
+//#5__Convert a Number to a String!
+function numberToString(num) { // создаем функцию
+    let a = num.toString(); // .toString преобразует элемент (все элементы массива) в строковое значение
+    return a; // выводит результат
+}
